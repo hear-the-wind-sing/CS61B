49ddc43bdc5bb71dd3e30d85da04041b8c66d69f
hear-the-wind-sing <2514872803@qq.com>
2024-04-23 14:23:17 +0800

    lab4_Flik

--- lab4/flik/HorribleSteve.java
+++ lab4/flik/HorribleSteve.java
@@ -11,4 +11,14 @@ public class HorribleSteve {
         }
         System.out.println("i is " + i);
     }
+//    public static void main(String [] args) throws Exception {
+//        Integer i = new Integer(0);
+//        for (Integer j = new Integer(0); i < 500; ++i, ++j) {
+//            if (!Flik.isSameNumber(i, j)) {
+//                throw new Exception(
+//                        String.format("i:%d not same as j:%d ??", i, j));
+//            }
+//        }
+//        System.out.println("i is " + i);
+//    }
 }
