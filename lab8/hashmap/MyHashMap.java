d1324e3fa97dda3ee53479a07b1163e6d6519e19
hear-the-wind-sing <2514872803@qq.com>
2024-04-26 23:36:43 +0800

    done with lab8

--- lab8/hashmap/MyHashMap.java
+++ lab8/hashmap/MyHashMap.java
@@ -1,6 +1,6 @@
 package hashmap;
 
-import java.util.Collection;
+import java.util.*;
 
 /**
  *  A hash table-backed Map implementation. Provides amortized constant time
@@ -28,11 +28,19 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
     /* Instance Variables */
     private Collection<Node>[] buckets;
     // You should probably define some more!
+    private int size = 0;
+
+    private int INITIALSIZE = 16;
+    private double LOADFACTOR = 0.75;
 
     /** Constructors */
-    public MyHashMap() { }
+    public MyHashMap() {
+        buckets = createTable(INITIALSIZE);
+    }
 
-    public MyHashMap(int initialSize) { }
+    public MyHashMap(int initialSize) {
+        buckets = createTable(initialSize);
+    }
 
     /**
      * MyHashMap constructor that creates a backing array of initialSize.
@@ -41,13 +49,17 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * @param initialSize initial size of backing array
      * @param maxLoad maximum load factor
      */
-    public MyHashMap(int initialSize, double maxLoad) { }
+    public MyHashMap(int initialSize, double maxLoad) {
+        LOADFACTOR = maxLoad;
+        buckets = createTable(initialSize);
+    }
 
     /**
      * Returns a new node to be placed in a hash table bucket
      */
     private Node createNode(K key, V value) {
-        return null;
+        return new Node(key, value);
+        //        return null;
     }
 
     /**
@@ -69,7 +81,8 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * OWN BUCKET DATA STRUCTURES WITH THE NEW OPERATOR!
      */
     protected Collection<Node> createBucket() {
-        return null;
+        return new LinkedList<>();
+        //return null;
     }
 
     /**
@@ -82,10 +95,104 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * @param tableSize the size of the table to create
      */
     private Collection<Node>[] createTable(int tableSize) {
-        return null;
+        Collection<Node>[] arrayOfCollections = new Collection[tableSize];
+        for (int i = 0; i < tableSize; i++) {
+            arrayOfCollections[i] = createBucket();
+        }
+        return arrayOfCollections;
+        //return null;
     }
 
     // TODO: Implement the methods of the Map61B Interface below
     // Your code won't compile until you do so!
-
+    public void clear() {
+        size = 0;
+        buckets = createTable(INITIALSIZE);
+        return;
+    }
+    public boolean containsKey(K key) {
+        int index = Math.floorMod(key.hashCode(), buckets.length);
+        for(Node node : buckets[index]) {
+            if(node.key.equals(key)) {
+                return true;
+            }
+        }
+        return false;
+        //throw new UnsupportedOperationException("This operation is not supported.");
+    }
+    public V get(K key) {
+        int index = Math.floorMod(key.hashCode(), buckets.length);
+        for(Node node : buckets[index]) {
+            if(node.key.equals(key)) {
+                return node.value;
+            }
+        }
+        return null;
+        //throw new UnsupportedOperationException("This operation is not supported.");
+    }
+    public int size() {
+        return size;
+    }
+    public void put(K key, V value) {
+        int index = Math.floorMod(key.hashCode(), buckets.length);
+        for (Node node : buckets[index]) {
+            if (node.key.equals(key)) {
+               node.value = value;
+               return;
+            }
+        }
+        buckets[index].add(createNode(key, value));
+        size++;
+        if ((double)(size / buckets.length) > LOADFACTOR) {
+            resize(buckets.length * 2);
+        }
+//        throw new UnsupportedOperationException("This operation is not supported.");
+    }
+    public void resize(int  capacity) {
+        Set<Node> nodeSet = nodeSet();
+        buckets = createTable(capacity);
+        size = 0;
+        for(Node node: nodeSet){
+            put(node.key,node.value);
+        }
+    }
+    public Set<Node> nodeSet() {
+        HashSet<Node> nodeSet = new HashSet<>();
+        for (int i = 0; i < buckets.length; i++) {
+            for(Node node : buckets[i]){
+                nodeSet.add(node);
+            }
+        }
+        return nodeSet;
+    }
+    public Set<K> keySet() {
+        HashSet<K> kSet = new HashSet<>();
+        for (int i = 0; i < buckets.length; i++) {
+            for(Node node : buckets[i]){
+                kSet.add(node.key);
+            }
+        }
+        return kSet;
+        //throw new UnsupportedOperationException("This operation is not supported.");
+    }
+    public V remove(K key) {
+        throw new UnsupportedOperationException("This operation is not supported.");
+    }
+    public V remove(K key, V value) {
+        throw new UnsupportedOperationException("This operation is not supported.");
+    }
+    public Iterator<K> iterator(){
+        return keySet().iterator();
+//        return new MyHashMapIterator();
+        //throw new UnsupportedOperationException("This operation is not supported.");
+    }
+//    private class MyHashMapIterator implements Iterator<K> {
+//        private Iterator<K> iterator = buckets[0].iterator();
+//        public boolean hasNext(){
+//
+//        }
+//        public K next(){
+//
+//        }
+//    }
 }
